0864715dd067ad007b9bc7fd1dc7b8f12e06f846
Mathieu Bouvet <mathieubouvet42@gmail.com>
2022-04-08 16:38:34 +0200

    Install mb-react-data-table

--- src/pages/ViewEmployeesPage.jsx
+++ src/pages/ViewEmployeesPage.jsx
@@ -1,13 +1,25 @@
 import { useEmployeeList } from "../context/employeeListContext";
+import ReactDataTable from "mb-react-data-table";
 
 const ViewEmployeesPage = () => {
   const { employees } = useEmployeeList();
-  console.log(employees);
+
+  const headers = [
+    {
+      name: "First Name",
+      dataKey: "firstName",
+    },
+    {
+      name: "Last Name",
+      dataKey: "lastName",
+    },
+  ];
   return (
     <>
       <header>
         <h2>Current Employees</h2>
       </header>
+      <ReactDataTable columns={headers} initialEntriesNumber={2}>{employees}</ReactDataTable>
     </>
   );
 };
